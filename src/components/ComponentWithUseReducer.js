7da533f88ac6729bd8fbc782cbc3f14040440334
cw <fromnowwon@gmail.com>
2024-02-12 20:17:24 +0900

    create action

--- src/components/ComponentWithUseReducer.js
+++ src/components/ComponentWithUseReducer.js
@@ -3,6 +3,12 @@ import React, { useReducer } from "react";
 const reducer = () => {};
 
 const ComponentWithUseReducer = () => {
+	const deposit = (amount) => {
+		dispatch({
+			type: "DEPOSIT",
+			payload: amount,
+		});
+	};
 	const [amount, dispatch] = useReducer(reducer, 500);
 	return <div>{amount}</div>;
 };
